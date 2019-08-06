45132ab173821e24a2008f6da87f02518b5a4e39
Rodrigo Dias Soares <rds_dx@hotmail.com>
2019-08-06 18:19:49 -0300

    inicio

---
+++ Course/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
